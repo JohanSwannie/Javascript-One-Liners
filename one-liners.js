e56ee35b0b01966c995a298c9de6475bda9ebc61
Johan Swan <jswannieman@gmail.com>
2023-09-29 13:45:29 +1300

    Commit one-liners Javascript Component

---
+++ one-liners.js
@@ -0,0 +1,39 @@
+//* Capitalize one word
+
+const oneWord = "potatoes";
+
+const capitalize = (word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+
+document.getElementById("p1").innerHTML = `${oneWord} now became ${capitalize(
+  oneWord
+)}`;
+
+//* Capitalize each word in a sentence
+
+const sentence = "the black bear eat potatoes";
+
+const words = sentence.split(" ");
+
+//* Longer Method
+
+for (let i = 0; i < words.length; i++) {
+  words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+}
+
+const newSentence1 = words.join(" ");
+
+document.getElementById(
+  "p2"
+).innerHTML = `Long Method - ${sentence} became ${newSentence1}`;
+
+//* Shorter Method
+
+const newSentence2 = words
+  .map((word) => {
+    return word[0].toUpperCase() + word.substring(1);
+  })
+  .join(" ");
+
+document.getElementById(
+  "p3"
+).innerHTML = `Shorter Method - ${sentence} became ${newSentence2}`;
