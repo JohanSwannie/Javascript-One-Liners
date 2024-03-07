5d4b8ba2ad3260e5b70f540011edd8ba56e5cf08
Johan Swan <jswannieman@gmail.com>
2024-03-07 13:29:44 +1300

    Update one-liners5 JS Component

--- one-liners5.js
+++ one-liners5.js
@@ -19,6 +19,10 @@ const obj1 = [
 
 let obj2 = obj1.sort((a, b) => (a["id"] > b["id"] ? 1 : -1));
 
+// OR
+
+// let obj2 = obj1.sort((a, b) => (a.id > b.id ? 1 : -1));
+
 for (let item of obj2) {
   for (let val in item) {
     console.log(`${val} : ${item[val]}`);
@@ -27,11 +31,25 @@ for (let item of obj2) {
 
 const array1 = [19, 33, 7, 28, 30, 17, 33, 20, 24, 19, 19, 7];
 
-alert([...new Set(array1)].sort((a, b) => a - b));
+console.log([...new Set(array1)].sort((a, b) => a - b));
 
 const array = [14, 67, 24, 7, 14, 12, 33, 7, 67];
-alert(
+
+console.log(
   array
     .filter((item, index, self) => self.indexOf(item) === index)
+    .sort((a, b) => a - b)
+);
+
+const array2 = [31, 29, 10, 45, 10, 12, 44, 31, 25, 29, 45, 7];
+
+console.log(
+  array2
+    .reduce((pValue, cValue) => {
+      if (!pValue.includes(cValue)) {
+        pValue.push(cValue);
+      }
+      return pValue;
+    }, [])
     .sort((a, b) => a - b)
 );
